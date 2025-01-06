09a720a68ecb0f82785ce0e411d4431ba817ef07
Erik Soma <stillusingirc@gmail.com>
2025-01-05 21:27:38 -0500

    monitors

--- .github/test-multiple-displays.sh
+++ .github/test-multiple-displays.sh
@@ -1,3 +1,6 @@
 xrandr
+xrandr --setmonitor screen0 800/211x600/159+0+0 screen
+xrandr --setmonitor screen1 800/212x600/159+800+0 none
+xrandr --listmonitors
 poetry run python .github/info.py
 poetry run pytest $@
