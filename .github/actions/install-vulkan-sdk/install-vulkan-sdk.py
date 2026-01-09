c4f2b0a088e0aba48942c15c71127125409f7a57
Erik Soma <stillusingirc@gmail.com>
2026-01-09 17:54:00 -0500

    #

---
+++ .github/actions/install-vulkan-sdk/install-vulkan-sdk.py
@@ -0,0 +1,62 @@
+import os
+import subprocess
+import sys
+from pathlib import Path
+from platform import system
+from tempfile import TemporaryDirectory
+from urllib.request import urlretrieve
+
+SDK_VERSION = sys.argv[1]
+
+if system() == "Windows":
+    SDK_URI = f"https://sdk.lunarg.com/sdk/download/{SDK_VERSION}/windows/vulkan_sdk.exe"
+elif system() == "Linux":
+    SDK_URI = f"https://sdk.lunarg.com/sdk/download/{SDK_VERSION}/linux/vulkan_sdk.tar.xz"
+elif system() == "Darwin":
+    SDK_URI = f"https://sdk.lunarg.com/sdk/download/{SDK_VERSION}/mac/vulkan_sdk.zip"
+else:
+    raise RuntimeError(f"unexpected system: {system()}")
+
+print(f"downloading vulkan sdk: {SDK_URI}")
+sdk_file_name, _ = urlretrieve(SDK_URI)
+
+os.mkdir("vulkan-sdk")
+
+if system() == "Windows":
+    subprocess.run(
+        [
+            sdk_file_name,
+            "--root",
+            os.abspath("vulkan-sdk"),
+            "--accept-licenses",
+            "--confirm-command",
+            "install",
+        ],
+        check=True,
+    )
+elif system() == "Linux":
+    subprocess.run(
+        ["sudo", "apt", "install", "libxcb-xinput0", "libxcb-xinerama0", "libxcb-cursor-dev"],
+        check=True,
+    )
+    subprocess.run(["tar", "-xf", sdk_file_name, "-C", "vulkan-sdk"], check=True)
+elif system() == "Darwin":
+    with TemporaryDirectory() as unzip_dir:
+        subprocess.run(["unzip", sdk_file_name, "-d", unzip_dir])
+        subprocess.run(
+            [
+                "sudo",
+                Path(unzip_dir) / f"Contents/MacOS/vulkansdk-macOS-{SDK_VERSION}",
+                "--root",
+                os.abspath("vulkan-sdk"),
+                "--accept-licenses",
+                "--confirm-command",
+                "install",
+            ],
+            check=True,
+        )
+else:
+    raise RuntimeError(f"unexpected system: {system()}")
+
+with open(os.environ["GITHUB_ENV"], "w") as github_env:
+    github_env.write(f"VULKAN_SDK={os.abspath('vulkan-sdk')}")
